648dd509d54f2daa7eb34267a76897ba8ddd8791
Fernando Jaramillo <f3rjara@gmail.com>
2023-03-16 14:09:40 -0500

    Add challenge No 1, JavaScript Drum Kit

---
+++ utils/keySounds.js
@@ -0,0 +1,47 @@
+export const keySounds = {
+  'a': {
+    sound: 'clap.wav',
+    color: '#60d394',
+    key : 65
+  },
+  's': {
+    sound: 'hihat.wav',
+    color: '#d36060',
+    key : 83
+  },
+  'd': {
+    sound: 'kick.wav',
+    color: '#c060d3',
+    key : 68
+  },
+  'f': {
+    sound: 'openhat.wav',
+    color: '#d3d160',
+    key : 70
+  },
+  'g': {
+    sound: 'boom.wav',
+    color: '#6860d3',
+    key : 71
+  },
+  'h': {
+    sound: 'ride.wav',
+    color: '#60b2d3',
+    key : 72
+  },
+  'j': {
+    sound: 'snare.wav',
+    color: '#d3b160',
+    key : 74
+  },
+  'k': {
+    sound: 'tom.wav',
+    color: '#d160d3',
+    key : 75
+  },
+  'l': {
+    sound: 'tink.wav',
+    color: '#c2d3d3',
+    key : 76
+  }
+};
