2221083dc18ee9140e154d875e9a27018bd77f42
Fernando Jaramillo <f3rjara@gmail.com>
2023-05-16 15:54:14 -0500

    Add challenge No 21

---
+++ challenges/21/app.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.log(err);
+    alert('Hey! You gotta allow that to happen!');
+  }
+);
